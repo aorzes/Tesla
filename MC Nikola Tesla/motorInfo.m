61ea73bc86289bd45628ba58ab01c66ea08ead69
Anton Orzes <anton.orzes@gmail.com>
2016-02-18 16:39:26 +0100

    trigo na motorinfo

--- MC Nikola Tesla/motorInfo.m
+++ MC Nikola Tesla/motorInfo.m
@@ -19,6 +19,51 @@
     // Do any additional setup after loading the view.
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+
+    CGSize velicina = self.view.frame.size;
+    UIImageView *stator =[[UIImageView alloc]init];
+    stator.image =[UIImage imageNamed:@"stator3"];
+    stator.frame = CGRectMake(velicina.width/6, velicina.height/2, velicina.width/1.5, velicina.width/1.5);
+    [self.view addSubview:stator];
+
+    UIImageView *trigonom =[[UIImageView alloc]init];
+    trigonom.image =[UIImage imageNamed:@"trigonom1"];
+    trigonom.frame = CGRectMake(velicina.width/10, stator.center.y+stator.frame.size.height/2+velicina.width/30, velicina.width/1.25, velicina.width/3.75);
+    trigonom.center = stator.center;
+    [self.view addSubview:trigonom];
+    trigonom.animationImages=[NSArray arrayWithObjects:
+                              [UIImage imageNamed: @"trigonom1"],
+                              [UIImage imageNamed: @"trigonom2"],
+                              [UIImage imageNamed: @"trigonom3"],
+                              [UIImage imageNamed: @"trigonom4"],
+                              [UIImage imageNamed: @"trigonom5"],
+                              [UIImage imageNamed: @"trigonom6"],
+                              [UIImage imageNamed: @"trigonom7"],
+                              [UIImage imageNamed: @"trigonom8"],
+                              [UIImage imageNamed: @"trigonom9"],
+                              [UIImage imageNamed: @"trigonom10"],
+                              [UIImage imageNamed: @"trigonom11"],
+                              [UIImage imageNamed: @"trigonom12"],
+                              [UIImage imageNamed: @"trigonom13"],
+                              [UIImage imageNamed: @"trigonom14"],
+                              [UIImage imageNamed: @"trigonom15"],
+                              [UIImage imageNamed: @"trigonom16"],
+                              [UIImage imageNamed: @"trigonom17"],
+                              [UIImage imageNamed: @"trigonom18"],
+                              [UIImage imageNamed: @"trigonom19"],
+                              [UIImage imageNamed: @"trigonom20"],
+                              [UIImage imageNamed: @"trigonom21"],
+                              [UIImage imageNamed: @"trigonom22"],
+                              [UIImage imageNamed: @"trigonom23"],
+                              [UIImage imageNamed: @"trigonom24"],
+                              nil];
+    trigonom.animationDuration=2.5;
+    trigonom.animationRepeatCount=0;
+    [trigonom startAnimating];
+
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
